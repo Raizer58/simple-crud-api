f50b55d8f177e482affb51e0d00d0677ad3a8dce
Maksim Slivin <raizer9058w@gmail.com>
2022-01-09 23:28:53 +0300

    feat: completed task

--- index.js
+++ index.js
@@ -1,41 +1,114 @@
 
 const http = require('http');
+const { v4, validate } = require('uuid');
+const { createReadStream, readFileSync, writeFileSync } = require('fs');
 
-const postData = JSON.stringify({
-  msg: 'Hello World!',
+const { PERSON } = require('./src/constants/path');
+
+const server = http.createServer((req, res) => {
+  const isLiquidURL = req.url.includes(PERSON);
+  const userId = isLiquidURL && req.url.replace(PERSON + '/', '');
+  const isNumberFormatId = (typeof userId === 'number' && !Number.isNaN(userId)) || validate(userId);
+
+  if (isLiquidURL && req.method === 'GET') {
+    if (isNumberFormatId) {
+      createReadStream('./data_BD.txt')
+        .on('data', (chunk) => {
+          const userInfo = JSON.parse(chunk)[userId];
+
+          if (userInfo) {
+            res.writeHead(200, {
+              'Content-Type': 'application/json',
+            }); 
+            res.end(JSON.stringify(userInfo)); 
+          } else {
+            res.writeHead(404); 
+            res.end(); 
+          }
+        });
+    } else {
+      createReadStream('./data_BD.txt')
+        .on('data', (chunk) => {
+
+          res.writeHead(200, {
+            'Content-Type': 'application/json',
+          }); 
+          res.end(JSON.stringify(JSON.parse(chunk))); 
+        });
+    }
+  } else if (isLiquidURL && req.method === 'POST') {
+    const chunks = [];
+
+    req.on('data', chunk => chunks.push(chunk));
+    req.on('end', () => {
+      const data = JSON.parse(Buffer.concat(chunks));
+      const userId = v4();
+
+      data.id = userId;
+
+      const currentData = JSON.parse(readFileSync('./data_BD.txt'));
+      const newData = {
+        ...currentData,
+        [userId]: {
+          ...data,
+        },
+      };
+      writeFileSync('./data_BD.txt', JSON.stringify(newData));
+
+      res.writeHead(202); 
+      res.end(); 
+    })    
+  } else if (isLiquidURL && isNumberFormatId && req.method === 'PUT') {
+    const chunks = [];
+
+    req.on('data', chunk => chunks.push(chunk));
+    req.on('end', () => {
+      const currentData = JSON.parse(readFileSync('./data_BD.txt'));
+      const isValidUserId = Boolean(Object.keys(currentData).find((v) => String(v) === String(userId)));
+
+      if (!isValidUserId) {
+        res.writeHead(404); 
+        res.end(); 
+
+        return;
+      }
+
+      const data = JSON.parse(Buffer.concat(chunks));
+
+      const newData = {
+        ...currentData,
+        [userId]: {
+          ...currentData[userId],
+          ...data,
+        },
+      };
+
+      writeFileSync('./data_BD.txt', JSON.stringify(newData));
+
+      res.writeHead(202); 
+      res.end(); 
+    }) 
+  } else if (isLiquidURL && isNumberFormatId && req.method === 'DELETE') {
+    const data = JSON.parse(readFileSync('./data_BD.txt'));
+    const isValidUserId = Boolean(Object.keys(data).find((v) => String(v) === String(userId)));
+    
+    if (!isValidUserId) {
+      res.writeHead(404); 
+      res.end(); 
+
+      return;
+    }
+
+    const { [userId]: deleteUserId, ...newData} = data;
+
+    writeFileSync('./data_BD.txt', JSON.stringify(newData));
+
+    res.writeHead(202); 
+    res.end(); 
+  } else {
+    res.writeHead(400); 
+    res.end();
+  }
 });
 
-const options = {
-  hostname: 'localhost',
-  port: 10000,
-  path: '/person',
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-    'Content-Length': Buffer.byteLength(postData),
-  },
-};
-
-const req = http.request('http://localhost:10000/person', (res) => {
-  console.log(`STATUS: ${res.statusCode}`);
-  console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-  res.setEncoding('utf8');
-  res.on('data', (chunk) => {
-    console.log(`BODY: ${chunk}`);
-  });
-  res.on('end', () => {
-    console.log('No more data in response.');
-  });
-});
-
-req.on('error', (e) => {
-  console.error(`problem with request: ${e.message}`);
-});
-
-// Write data to request body
-req.write(postData);
-req.end();
-
-const server = http.createServer((res, req) => {});
-
-server.listen(10000);
+server.listen(9000);
